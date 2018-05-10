affd361f98761eb54e7a2c7aff97f98bf2a74547
Vision-X <michael.s.marlow@gmail.com>
2018-05-10 15:37:17 -0600

    build placeholder content for Landing page component

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -4,9 +4,18 @@ class Landing extends Component {
   render() {
     return (
       <section>
-
-
-
+        <div>
+          <h2>Front-End</h2>
+        </div>
+        <div>
+          <h2>Back-End</h2>
+        </div>
+        <div>
+          <h2>Servers</h2>
+        </div>
+        <div>
+          <h2>Libraries</h2>
+        </div>
       </section>
     )
   }
