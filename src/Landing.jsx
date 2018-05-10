cbe4c9ccb249ca4bb2f1bee3a1040a80d1cb55bf
Vision-X <michael.s.marlow@gmail.com>
2018-05-10 15:32:07 -0600

    add Landing component and imports for index

---
+++ src/Landing.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Landing extends Component {
+  render() {
+    return (
+      <section>
+
+
+
+      </section>
+    )
+  }
+}
+
+
+
+
+
+
+export default Landing;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+import Header from './Header.jsx';
+import Landing from './Landing.jsx';
+
+
+
 ReactDOM.render(
+  <Router>
+    <div>
+      <Header />
+
+    </div>
+  </Router>
   <App />,
 document.getElementById('root'));
 registerServiceWorker();
