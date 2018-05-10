4422744500fb3a5c7ed5772ea4c446a366f26f57
Vision-X <michael.s.marlow@gmail.com>
2018-05-10 15:59:03 -0600

    add AddResource component with form for submitting content, dropdown for selecting topic

---
+++ src/AddResource.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+class AddResource extends Component {
+  constructor() {
+    super();
+    this.state = { }
+  }
+
+  render() {
+    return (
+      <form>
+        <label>Select a category:</label>
+        <select name="category">
+          <option value="Front-End"></option>
+          <option value="Back-End"></option>
+          <option value="Servers"></option>
+          <option value="Libraries"></option>
+        </select>
+
+
+        <label>Add a resource:</label>
+
+        <input type="text" name="" />
+
+      </form>
+    );
+  }
+}
+
+export default AddResource;
