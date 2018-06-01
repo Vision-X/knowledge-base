2a8402d99cf444cb38bc81db9f56066e85a27ca2
Vision-X <michael.s.marlow@gmail.com>
2018-06-01 16:47:49 -0600

    create BackEnd container component

---
+++ src/BackEnd.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import Category from './Category';
+
+class FrontEnd extends Component {
+  render() {
+    const { data, categoryData, onClick } = this.props;
+    return (
+      <div onClick={onClick}>
+        <div className="content-card-title">
+          <h2>Front-End</h2>
+        </div>
+        <div className="content-card-list">
+          <ul>
+            {categoryData.map(category => {
+                return (
+                  <li key={category.indexOf()}>
+                    <Category
+                      category={category}
+                      target="_blank"
+                    >
+                      {category}
+                    </Category>
+                  </li>
+              )
+            })
+          }
+          </ul>
+        </div>
+        <div className="add-btn">
+          <a href="" target="_blank" />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default FrontEnd;
